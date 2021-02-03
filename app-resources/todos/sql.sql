7cd6f6986694f867e76d0278cc4f2742199f30d8
landinialejandro <landinialejandro@hotmail.com>
2021-02-03 20:32:36 -0300

    fix create tables todos

--- app-resources/todos/sql.sql
+++ app-resources/todos/sql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `prueba`.`landini_todo` (
+CREATE TABLE IF NOT EXISTS `landini_todo` (
   `memberID` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `todos` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`memberID`)
